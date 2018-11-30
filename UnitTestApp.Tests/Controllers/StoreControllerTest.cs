4b4b862d5da3143038f9a453cd5d525cc7331fc3
EvanBS <evandiesp@gmail.com>
2018-11-30 21:10:37 +0200

    unit tests

---
+++ UnitTestApp.Tests/Controllers/StoreControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestApp.Controllers;
+
+namespace UnitTestApp.Tests.Controllers
+{
+    [TestClass]
+    public class StoreControllerTest
+    {
+        [TestMethod]
+        public void IndexViewResultNotNull()
+        {
+            // Arrange
+            StoreController storeController = new StoreController();
+
+            // Act
+            ViewResult result = storeController.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void TestMessage()
+        {
+            // Arrange
+            StoreController storeController = new StoreController();
+
+            // Act
+            ViewResult result = storeController.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Another message", result.ViewBag.Message);
+        }
+    }
+}
